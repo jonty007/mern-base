83d50f08e0852689e8355462ad399d5a7ff9278a
vivek singh <vivek.singh@quanteon.in>
2021-06-29 01:05:45 +0530

    Profile pic update Profile info update

---
+++ server/v1/file/file.controller.js
@@ -0,0 +1,47 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+// To parse file data
+const upload = multer();
+
+const file = Router();
+
+/**
+ * @api {post} /file/profile-upload
+ * @apiName Profile upload
+ * @apiGroup FIle
+ *
+ * @apiHeader {String} authorization Users unique access-key.
+ * @apiHeader {String} Accept-Language language to get response for any messages from API. default to en (english)
+ *
+ * @apiParam (FormData) {File} profile Profile picture
+ *
+ * @apiParamExample  {FormData} Request-Example:
+ * {
+ *  "profile": "file"
+ * }
+ *
+ * @apiSuccessExample {json} Success-Response:
+ * {
+ *   data: {
+ *    fileId: "60d886870dcace6be6826d90"
+ *  }
+ * }
+ *
+ * @apiError (Error 400) ValidationError Validation failed!
+ *
+ */
+file.post('/file/profile-upload', upload.single('profile'), async (req, res, next) => {
+  try {
+    const uploadedFile = await saveProfileFile(req.file);
+
+    return res.send({ data: { fileId: uploadedFile._id.toString() } });
+  } catch (e) {
+    if (e.message) {
+      return res.status(405).send({
+        message: e.message
+      });
+    }
+    return next(e);
+  }
+});
--- server/v1/user/user.service.js
+++ server/v1/user/user.service.js
@@ -13,6 +13,39 @@ export async function getUserProfilePicture(userId) {
   return info;
 }
 
+export async function updateUserProfile(userId, data) {
+  const {
+    firstName,
+    lastName,
+    phone,
+    dob,
+    profileImageId
+  } = data;
+
+  if (!firstName || !validator.isAlpha(firstName)) {
+    throw new Error('USER.UPDATE.REQUIRED_FIRST_NAME');
+  }
+
+  if (!dob || !moment(dob, 'YYYY-MM-DD').isValid()) {
+    throw new Error('USER.UPDATE.REQUIRED_DOB');
+  }
+
+  if (!profileImageId) {
+    throw new Error('USER.UPDATE.REQUIRED_PROFILE_IMAGE');
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(userId, {
+    firstName,
+    lastName,
+    phone,
+    dob,
+    profileImageId
+  });
+
+  return convertToUserDTO(updatedUser);
+
+}
+
 function convertToUserDTO(user) {
   return {
     _id: user._id.toString(),
